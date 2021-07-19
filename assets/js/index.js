26ac6fa99f2172050b8027921bd8486b5ed8606c
puxinman <526766735@qq.com>
2021-07-19 21:40:44 +0800

    完成文章管理相关功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 $(function() {
     //调用函数 获取用户基本信息
-    getUSerInfo()
+    getUserInfo()
 
     var layer = layui.layer
         //退出功能
@@ -18,56 +18,62 @@ $(function() {
         });
     })
 
+})
 
-    //获取用户基本信息
-    function getUSerInfo() {
-        $.ajax({
-            url: '/my/userinfo',
-            method: 'GET',
-            //headers 就是请求头配置对象
-            // headers: {
-            //     Authorization: localStorage.getItem('token') || ''
-            // },
-            success: function(res) {
-                console.log(res);
-                if (res.status !== 0) {
-                    return layui.layer.msg('获取用户权限失败！')
-                }
-                //调用函数渲染用户头像
-                renderAvatar(res.data)
-            },
-            //无论成功还是失败，都会调用complete回调函数
-            // complete: function(res) {
-            //     console.log(res);
-            //     // layer.msg(11)
-            //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-            //         //强制清空token数据
-            //         localStorage.removeItem('token')
-            //             //强制回到登录界面
-            //         location.href = 'login.html'
 
-            //     }
 
-            // }
-        })
-    }
+//获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        method: 'GET',
+        //headers 就是请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            // console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户权限失败！')
+            }
+            //调用函数渲染用户头像
+            renderAvatar(res.data)
+        },
+        //无论成功还是失败，都会调用complete回调函数
+        // complete: function(res) {
+        //     console.log(res);
+        //     // layer.msg(11)
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         //强制清空token数据
+        //         localStorage.removeItem('token')
+        //             //强制回到登录界面
+        //         location.href = 'login.html'
 
-    //渲染用户头像
-    function renderAvatar(user) {
-        //1.获取用户名称
-        var name = user.nickname || user.username
-            //设置欢迎的文本
-        $('.welcome').html('欢迎&nbsp;&nbsp;' + name)
-        var pic = user.user_pic
-        if (pic !== null) {
-            //渲染头像
-            $('.layui-nav-img').attr('src', pic).show()
-            $('.text-avatar').hide()
-        }
+        //     }
+
+        // }
+    })
+}
+
+//渲染用户头像
+function renderAvatar(user) {
+    //1.获取用户名称
+    var name = user.nickname || user.username
+        //设置欢迎的文本
+    $('.welcome').html('欢迎&nbsp;&nbsp;' + name)
+    var pic = user.user_pic
+    if (pic !== null) {
+        //渲染头像
+        $('.text-avatar').hide()
+        $('.layui-nav-img').attr('src', pic).show()
+
+    } else {
         //渲染文本图片
         $('.layui-nav-img').hide()
         var first = name[0].toUpperCase()
         $('.text-avatar').html(first).show()
-
+        console.log(user);
     }
-})
+
+
+}
